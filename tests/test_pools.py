52dca6c17ec5bb0c14570bd4cb997dbef8185370
Claudio Netto <nettinhorama@gmail.com>
2019-02-22 19:31:14 -0300

    add list pools method

--- tests/test_pools.py
+++ tests/test_pools.py
@@ -1,19 +1,55 @@
 from tsuruclient import client
 
+import json
 import unittest
 import httpretty
 
 
 class PoolsTestCase(unittest.TestCase):
     def setUp(self):
+        self.target = "https://target.example.com"
+        self.token = "example-of-token"
+        self.client = client.Client(self.target, self.token)
         httpretty.enable()
 
     def tearDown(self):
         httpretty.disable()
         httpretty.reset()
 
+    def test_list_when_server_return_no_content_status(self):
+        url = "{}/pools".format(self.target)
+        httpretty.register_uri(httpretty.GET, url, status=204)
+
+        pools = self.client.pools.list()
+        
+        expected_pools = []
+        self.assertListEqual(pools, expected_pools)
+
+    def test_list(self):
+        expected_pools = [
+            {
+                "allowed": {
+                    "plans": ["medium"],
+                    "router": ["hipache"],
+                    "service": ["healthcheck"],
+                    "team": ["theoneteam"]
+                },
+                "default": False,
+                "public": True,
+                "name": "theonepool",
+                "teams": ["theoneteam"]
+            }
+        ]
+
+        url = "{}/pools".format(self.target)
+        httpretty.register_uri(httpretty.GET, url, status=200, body=json.dumps(expected_pools))
+
+        pools = self.client.pools.list()
+        
+        self.assertListEqual(pools, expected_pools)
+
     def test_rebalance(self):
-        url = "http://target/node/rebalance"
+        url = "{}/node/rebalance".format(self.target)
         httpretty.register_uri(
             httpretty.POST,
             url,
@@ -21,8 +57,7 @@ class PoolsTestCase(unittest.TestCase):
             status=200
         )
 
-        cl = client.Client("http://target", "abc123")
-        cl.pools.rebalance("dev")
+        self.client.pools.rebalance("dev")
 
         result = httpretty.last_request().body.decode('utf-8')
         expected = "MetadataFilter.pool=dev"
--- tsuruclient/pools.py
+++ tsuruclient/pools.py
@@ -5,6 +5,12 @@ class Manager(Base):
     """
     Manage pool resources.
     """
+    def list(self):
+        """
+        List pools.
+        """
+        return self.request("get", "/pools",
+            handle_response=lambda response: [] if response.status_code == 204 else response.json())
 
     def rebalance(self, pool):
         """
